3255c9e276a046d2d4f04d68477a97f633fc11ff
Rahul Yadav <34509120+Rahulyadav9@users.noreply.github.com>
2024-12-03 18:10:59 +0530

    Update about.js

--- Client/src/component/about.js
+++ Client/src/component/about.js
@@ -1,1 +1,23 @@
 
+// src/pages/AboutPage.js
+
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h2>IKEA is a global leader in life at home. </h2>
+      <p>
+        `Whether you just moved into a new home or looking to revamp your current one, we at IKEA are here to inspire you with affordable home furniture solutions, there is a piece of furniture to every corner of your home. Create a home that is perfect for you.
+
+        Shopping at IKEA is a bit different and exciting compared to your shopping at an everyday retail. It is about experiencing solutions first hand and getting to know ideas and inspirations that can fit perfectly into your home. That’s why, we offer more than 7500 products, solutions at our store along with home furnishing ideas and services for you to explore.
+
+        When you visit IKEA store, make yourself at home in our many room settings in the store. Squeeze the upholsteries, feel the oriental rugs, try the sofa beds and open the cabinets to feel the quality. On the price tag, you’ll find all you need to know about a product, including where in the store you can pick it up.
+
+        Since most IKEA furniture is flat-packed, they are quite easy to bring home when you buy from the store.`
+        </p>
+    </div>
+  );
+};
+
+export default AboutPage;
